3de7d72b5f2b18e90221607a344ac717b351af21
MelfQuin <jordanholmes@gmail.com>
2022-11-23 19:52:01 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./_includes/*.html",
+    "./_layouts/*.html",
+    "./_pages/*.html",
+    "./_posts/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
